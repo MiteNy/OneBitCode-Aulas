b3892b2bc8955d4fda13c9ab13e5116882afc051
Cristian Rocha <cristianrocha839@gmail.com>
2024-12-28 16:56:35 -0300

    escala

--- JavaScript2/exercicio1/fila.js
+++ JavaScript2/exercicio1/fila.js
@@ -1,36 +1,26 @@
-let menu
-let ordem =""
-let filaPacientes = ["João","Marcos","Tiago"]
-let i=0
-do{
-    
-    // if(i==filaPacientes.length-1){
-    //     ordem=""
-    // }
-    // i=0
+// function somaNumeros(arr){
+//     let valor = arr.reduce((valorAtual,index)=> valorAtual+index)
+//     return valor
+// }
+// console.log(somaNumeros([1, 2, 3, 4, 5]))
+// console.log(somaNumeros([10, -2, 7]))
 
-    for(i=0;i<filaPacientes.length;i++){
-        ordem +=(i+1+"º Posição "+filaPacientes[i]+"\n")
-    }
+// -------------------------------------------------------------
 
-    menu = parseFloat(prompt("Ordem da fila: \n"+ordem+"\n----MENU---\n 1 - Novo Paciente \n 2 - Atender Paciente \n 3 - Sair"))
-    ordem =""
-    switch(menu){
-        case 1:
-            filaPacientes.push(prompt("Insira o nome do paciente que deseja adicionar a fila:"))
-            break
-        case 2:
-            if(filaPacientes.length ==0){
-                alert("Não tem mais nenhum paciente!")
-                break}else {alert(filaPacientes.shift() +" Será o próximo paciente")
-                    break
-                }
-        case 3:
-            alert("Saindo...")
-            break
-        default:
-            alert("nenhum dos numeros")
-    }
+// function concatenarStrings(string){
+//     return valor=string.reduce((acumulador,index)=> acumulador.concat(index+" "),"")
+// }
 
-}while (menu!= 3 || menu ==false);
-console.log(ordem)
+// console.log(concatenarStrings(['Eu', 'adoro', 'programar'])); // Saída: "Eu adoro programar"
+// console.log(concatenarStrings(['JavaScript', 'é', 'incrível'])); // Saída: "JavaScript é incrível"
+
+// --------------------------------------------------------------
+
+function produtoNumeros(valor){
+    return valor.reduce((acumulador,index)=> (acumulador*index),1)
+}
+
+console.log(produtoNumeros([2, 3, 4])); // Saída: 24
+console.log(produtoNumeros([]));        // Saída: 1
+
+52241136853125000292550010000005821000005836
